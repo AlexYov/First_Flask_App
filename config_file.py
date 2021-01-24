ca9b120e1cfc4655789fa0853fa0e9d2f86cf29f
AlexYov <xxxx81971@yandex.ru>
2021-01-24 22:27:10 +0500

    updates

--- config_file.py
+++ config_file.py
@@ -5,7 +5,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class BaseConfig(object):
     SECRET_KEY = 'sercret_key'
     POSTS_PER_PAGE = 5
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir+'/app', 'app.db')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     MAIL_SERVER = 'smtp.googlemail.com'
     MAIL_PORT = 587
