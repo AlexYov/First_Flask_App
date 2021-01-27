c9fcf0acef99e24baf8bc821605177435bfe830d
AlexYov <xxxx81971@yandex.ru>
2021-01-27 21:20:43 +0500

    ...

--- config_file.py
+++ config_file.py
@@ -1,11 +1,13 @@
 import os
+import psycopg2
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 class BaseConfig(object):
     SECRET_KEY = 'sercret_key'
     POSTS_PER_PAGE = 5
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'app.db')
+    #SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'app.db')
+    DATABASE_URL = os.environ['DATABASE_URL']
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     MAIL_SERVER = 'smtp.googlemail.com'
     MAIL_PORT = 587
@@ -13,5 +15,6 @@ class BaseConfig(object):
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     ADMINS = os.environ.get('ADMINS')
+    conn = psycopg2.connect(DATABASE_URL, sslmode='require') 
 
     
